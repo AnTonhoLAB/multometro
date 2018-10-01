399ac1b986a019fca89379f88816f410e48d00be
George Gomes <georgegomees@gmail.com>
2018-10-01 08:59:28 -0300

    Setup storyboard screens

---
+++ Mulltometro/Mulltometro/App/Groups/GroupsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GroupsViewController.swift
+//  Mulltometro
+//
+//  Created by George Gomes on 01/10/18.
+//  Copyright © 2018 CrowCode. All rights reserved.
+//
+
+import UIKit
+
+class GroupsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
