ff517854425fddcf2f88559600dcc712148c5bb3
George Gomes <georgegomees@gmail.com>
2018-10-02 08:27:28 -0300

    Set model classes

---
+++ Mulltometro/Mulltometro/NetWorking/CloudCommunication.swift
@@ -0,0 +1,11 @@
+//
+//  CloudCommunication.swift
+//  Mulltometro
+//
+//  Created by George Gomes on 02/10/18.
+//  Copyright © 2018 CrowCode. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import Rswift
