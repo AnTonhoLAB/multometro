74c30951a3542e7a8999660937dd58b4e2228edb
George Gomes <georgegomees@gmail.com>
2018-10-16 21:23:29 -0300

    Setup Register

---
+++ Mulltometro/Mulltometro/App/Login/RegisterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.swift
+//  Mulltometro
+//
+//  Created by George Gomes on 16/10/18.
+//  Copyright © 2018 CrowCode. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+}
