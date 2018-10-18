c5b4d71fa5edbc4d9fbdf4e796fbc4819d4b385b
George Gomes <georgegomees@gmail.com>
2018-10-18 15:54:03 -0300

    Localize AddGroupViewController strings

--- Mulltometro/Mulltometro/App/Groups/AddNewGroupViewController.swift
+++ Mulltometro/Mulltometro/App/Groups/AddNewGroupViewController.swift
@@ -7,8 +7,27 @@
 //
 
 import UIKit
+import Rswift
 
 class AddNewGroupViewController: UIViewController {
+    
+    @IBOutlet weak var btCancel: UIBarButtonItem! { didSet { btCancel.title = R.string.localizable.cancel() } }
+    @IBOutlet weak var btCreate: UIBarButtonItem! { didSet { btCreate.title = R.string.localizable.create() } }
+    
+    @IBOutlet weak var lbRoomName: UILabel!{ didSet { lbRoomName.text = R.string.localizable.roomName() } }
+    @IBOutlet weak var lbParticipate: UILabel!{ didSet { lbParticipate.text = R.string.localizable.willParticipate()} }
+    @IBOutlet weak var lbDueDate: UILabel!{ didSet { lbDueDate.text = R.string.localizable.dueDate() } }
+    
+    @IBOutlet weak var tfRoomName: UITextField!
+    @IBOutlet weak var stParticipate: UISwitch!
+    
+    @IBOutlet weak var dayPicker: UIPickerView!{
+        didSet{
+            dayPicker.dataSource = self
+            dayPicker.delegate = self
+            dayPicker.selectRow(4, inComponent: 0, animated: true)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +39,21 @@ class AddNewGroupViewController: UIViewController {
     
     @IBAction func didTapCreate(_ sender: Any) {
         
+    }
+}
+
+extension AddNewGroupViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 30
+    }
+}
+
+extension AddNewGroupViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(row + 1)
     }
 }
