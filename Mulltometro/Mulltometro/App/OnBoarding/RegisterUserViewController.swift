9c44d82be598e19e233b93a86d45a6bf37537b00
George Gomes <georgegomees@gmail.com>
2018-12-05 10:15:10 -0200

    Removed keyboard animation in on boarding (to complete the name)

--- Mulltometro/Mulltometro/App/OnBoarding/RegisterUserViewController.swift
+++ Mulltometro/Mulltometro/App/OnBoarding/RegisterUserViewController.swift
@@ -37,9 +37,6 @@ class RegisterUserViewController: UIViewController {
     private let roundRadius: CGFloat = 8.0
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -85,30 +82,5 @@ class RegisterUserViewController: UIViewController {
         UIView.transition(with: window, duration: 0.4, options: .transitionCrossDissolve, animations: {
             window.rootViewController = tabBarController
         }, completion: nil)
-    }
-}
-
-// MARK: - Keyboard
-
-extension RegisterUserViewController {
-    @objc func keyboardWillShow(sender: NSNotification) {
-        let sizeToKeyboard = view.frame.maxY - fieldsView.frame.maxY
-        if let keyboardSize = (sender.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
-            let keyBoardheight = keyboardSize.height
-            UIView.animate(withDuration: 0.8) { [weak self] in
-                guard let self = self else { return }
-                let constant = (sizeToKeyboard - keyBoardheight)
-                self.constraintToBot.constant = Swift.abs(constant) + 40
-                self.view.layoutIfNeeded()
-            }
-        }
-    }
-    
-    @objc func keyboardWillHide(sender: NSNotification) {
-        UIView.animate(withDuration: 0.8) { [weak self] in
-            guard let self = self else { return }
-            self.constraintToBot.constant = 0
-            self.view.layoutIfNeeded()
-        }
     }
 }
